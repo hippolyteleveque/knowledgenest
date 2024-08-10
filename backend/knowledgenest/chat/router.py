d7e15df54acbf3d1f02e441b7d6a71efcc909ed6
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-08-10 17:55:06 +0200

    Added the backend endpoints to handle the conversation.

--- backend/knowledgenest/chat/router.py
+++ backend/knowledgenest/chat/router.py
@@ -1,13 +1,49 @@
+from typing import List
 from fastapi import APIRouter
 from knowledgenest.database import DbSession
-from knowledgenest.chat.schema import ChatMessageIn, ChatMessageOut
-from knowledgenest.chat.service import chat
+from knowledgenest.chat.schema import (
+    ChatConversationOut,
+    ChatMessageIn,
+    ChatMessageOut,
+    FirstChatMessageOut,
+)
+from knowledgenest.chat.service import (
+    chat,
+    add_conversation,
+    fetch_conversation,
+    fetch_conversations,
+)
 
 
 router = APIRouter(prefix="/chat", tags=["chat"])
 
 
-@router.post("/", response_model=ChatMessageOut)
-def send_message(request: ChatMessageIn, db: DbSession):
-    message = chat(request.message, db)
-    return ChatMessageOut(message=message)
+@router.post("/", response_model=FirstChatMessageOut)
+def create_convesation(request: ChatMessageIn, db: DbSession):
+    conversation = add_conversation(db)
+    message = chat(request.message, conversation.id, db)
+    return dict(message=message, type="ai", conversation_id=conversation.id)
+
+
+@router.get("/", response_model=List[ChatConversationOut])
+def get_conversations(db: DbSession):
+    conversations = fetch_conversations(db)
+    formatted_conversations = [
+        ChatConversationOut(id=conversation.id) for conversation in conversations
+    ]
+    return formatted_conversations
+
+
+@router.get("/{id}", response_model=List[ChatMessageOut])
+def get_conversation(id: int, db: DbSession):
+    messages = fetch_conversation(id, db)
+    formatted_messages = [
+        ChatMessageOut(message=msg.content, type=msg.type) for msg in messages
+    ]
+    return formatted_messages
+
+
+@router.post("/{id}", response_model=ChatMessageOut)
+def send_chat(id: int, request: ChatMessageIn, db: DbSession):
+    message = chat(request.message, id, db)
+    return ChatMessageOut(message=message, type="ai")
--- backend/knowledgenest/chat/service.py
+++ backend/knowledgenest/chat/service.py
@@ -1,9 +1,10 @@
 import os
+from typing import List
 from sqlalchemy import asc
 from sqlalchemy.orm.session import Session
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-from knowledgenest.chat.models import ChatMessage
+from knowledgenest.chat.models import ChatConversation, ChatMessage
 
 from datetime import datetime
 
@@ -20,30 +21,65 @@ def send_llm_message(message: str) -> str:
     return resp
 
 
-def chat(new_message: str, db: Session) -> str:
+def fetch_conversation(conversation_id: int, db: Session) -> List[ChatMessage]:
+    db_conversation = (
+        db.query(ChatMessage)
+        .filter(ChatMessage.conversation_id == conversation_id)
+        .order_by(asc(ChatMessage.created_at))
+        .all()
+    )
+    return db_conversation
+
+
+def fetch_conversations(db: Session) -> List[ChatConversation]:
+    conversations = (
+        db.query(ChatConversation).order_by(asc(ChatConversation.created_at)).all()
+    )
+    return conversations
+
+
+def chat(new_message: str, conversation_id: int, db: Session) -> str:
     """Continue the chat with the user"""
-    add_human_message(new_message, db)
-    db_messages = db.query(ChatMessage).order_by(asc(ChatMessage.created_at)).all()
-    messages = [msg.convert_to_langchain() for msg in db_messages]
-    print(messages)
+    add_human_message(new_message, conversation_id, db)
+    db_conversation = fetch_conversation(conversation_id, db)
+    messages = [msg.convert_to_langchain() for msg in db_conversation]
     llm = ChatOpenAI(model=MODEL, api_key=OPENAI_API_KEY)
     chain = llm | StrOutputParser()
     resp = chain.invoke(messages)
-    add_ai_message(resp, db)
+    add_ai_message(resp, conversation_id, db)
     return resp
 
 
-def add_human_message(content: str, db: Session):
-    new_message = ChatMessage(content=content, type="human", created_at=datetime.now())
+def add_human_message(content: str, conversation_id: int, db: Session):
+    new_message = ChatMessage(
+        content=content,
+        type="human",
+        created_at=datetime.now(),
+        conversation_id=conversation_id,
+    )
     db.add(new_message)
     db.commit()
     db.refresh(new_message)
     return new_message
 
 
-def add_ai_message(content: str, db: Session):
-    new_message = ChatMessage(content=content, type="ai", created_at=datetime.now())
+def add_ai_message(content: str, conversation_id: int, db: Session):
+    new_message = ChatMessage(
+        content=content,
+        type="ai",
+        created_at=datetime.now(),
+        conversation_id=conversation_id,
+    )
     db.add(new_message)
     db.commit()
     db.refresh(new_message)
     return new_message
+
+
+def add_conversation(db: Session):
+    """Create a new conversation"""
+    new_conversation = ChatConversation(created_at=datetime.now())
+    db.add(new_conversation)
+    db.commit()
+    db.refresh(new_conversation)
+    return new_conversation
