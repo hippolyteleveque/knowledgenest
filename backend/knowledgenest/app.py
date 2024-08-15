9e36409d967e6dde29d9bc93e95106dd8a2f3c5e
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-08-15 14:25:30 +0200

    Added health check and psycopg2 to the libraries.

--- backend/knowledgenest/app.py
+++ backend/knowledgenest/app.py
@@ -1,15 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from knowledgenest.auth.router import router as auth_router
 from knowledgenest.articles.router import router as articles_router
 from knowledgenest.chat.router import router as chat_router
+from starlette.status import HTTP_200_OK
 
-app = FastAPI(root_path="/api")
+app = FastAPI(root_path="/api/v1")
 
 app.include_router(auth_router)
 app.include_router(articles_router)
 app.include_router(chat_router)
 
 
-@app.get("/api")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/health")
+def health():
+    return Response(status_code=HTTP_200_OK)
