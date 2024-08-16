0523f0467bf1ff11972d9c70ce0c0d7eacf153aa
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-08-16 16:54:32 +0200

    Replaced OPENAI with MISTRALAI

--- backend/knowledgenest/lib.py
+++ backend/knowledgenest/lib.py
@@ -1,6 +1,9 @@
 import os
 
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-EMBEDDING_MODEL = "text-embedding-ada-002"
-MODEL = "gpt-4o-mini"
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# EMBEDDING_MODEL = "text-embedding-ada-002"
+# MODEL = "gpt-4o-mini"
+MISTRALAI_API_KEY = os.getenv("MISTRALAI_API_KEY")
+MISTRAL_EMBEDDING_MODEL = "mistral-embed"
+MISTRAL_LLM_MODEL = "open-mistral-nemo"
