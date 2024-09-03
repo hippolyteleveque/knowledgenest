7b12ba6894e73e2f1a62792f3a9379d5ee20adce
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-09-03 17:31:55 +0200

    Updated extension code so that it takes into account token expiration date.

--- chrome/background.js
+++ chrome/background.js
@@ -1,10 +1,14 @@
-import { sendActiveUrl } from "./lib.js";
+import { sendActiveUrl, checkTokenValidity } from "./lib.js";
 
 chrome.action.onClicked.addListener(handleActionClick);
 
 function handleActionClick(tab) {
-  chrome.storage.sync.get("kn-jwtToken", function (data) {
-    if (data["kn-jwtToken"]) {
+  chrome.storage.sync.get(["kn-jwtToken", "kn-token-exp"], function (data) {
+    if (
+      data["kn-jwtToken"] &&
+      data["kn-token-exp"] &&
+      checkTokenValidity(data["kn-token-exp"])
+    ) {
       sendActiveUrl(data["kn-jwtToken"]);
       // Remove popup to allow future clicks to trigger the listener
       chrome.action.setPopup({ popup: "" });
