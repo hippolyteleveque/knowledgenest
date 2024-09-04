915e0ccbbd94ad06cbce8bbb6cd8e524d4258a4b
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-09-04 22:33:57 +0200

    Added popups for successes and failures of adding articles.

--- chrome/background.js
+++ chrome/background.js
@@ -3,18 +3,27 @@ import { sendActiveUrl, checkTokenValidity } from "./lib.js";
 chrome.action.onClicked.addListener(handleActionClick);
 
 function handleActionClick(tab) {
-  chrome.storage.sync.get(["kn-jwtToken", "kn-token-exp"], function (data) {
-    if (
-      data["kn-jwtToken"] &&
-      data["kn-token-exp"] &&
-      checkTokenValidity(data["kn-token-exp"])
-    ) {
-      sendActiveUrl(data["kn-jwtToken"]);
-      // Remove popup to allow future clicks to trigger the listener
-      chrome.action.setPopup({ popup: "" });
-    } else {
-      chrome.action.setPopup({ popup: "popup.html" });
-      chrome.action.openPopup();
+  chrome.storage.sync.get(
+    ["kn-jwtToken", "kn-token-exp"],
+    async function (data) {
+      if (
+        data["kn-jwtToken"] &&
+        data["kn-token-exp"] &&
+        checkTokenValidity(data["kn-token-exp"])
+      ) {
+        const resp = await sendActiveUrl(data["kn-jwtToken"]);
+        // Remove popup to allow future clicks to trigger the listener
+        if (resp.ok) {
+          chrome.action.setPopup({ popup: "success-popup.html" });
+          chrome.action.openPopup();
+        } else {
+          chrome.action.setPopup({ popup: "failure-popup.html" });
+          chrome.action.openPopup();
+        }
+      } else {
+        chrome.action.setPopup({ popup: "login-popup.html" });
+        chrome.action.openPopup();
+      }
     }
-  });
+  );
 }
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,11 +1,8 @@
-import { login } from "./lib.js";
-
 document.addEventListener("DOMContentLoaded", function () {
-  const authForm = document.getElementById("kn-authForm");
-  const frame = document.getElementById("kn-frame");
-  authForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const formData = new FormData(authForm);
-    login(formData);
-  });
+  setTimeout(() => {
+    chrome.action.setPopup({ popup: "" });
+    window.close();
+  }, 3000);
 });
+
+
