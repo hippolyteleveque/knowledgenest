856daabe3f54747ed4b88d8650b0f45c5f5a4cbe
Hippolyte Lévêque <hippolyte.leveque@essec.edu>
2024-09-16 11:37:05 +0200

    Cleaned the pinecone idx initialization function.

--- backend/knowledgenest/vector_database.py
+++ backend/knowledgenest/vector_database.py
@@ -13,30 +13,41 @@ PINECONE_INDEX_NAME = config.PINECONE_INDEX_NAME
 EMBEDDING_MODEL_DIM = 1024
 SIM_METRIC = "dotproduct"
 
-pc = None
+idx: Index | None = None
 
 
-def initialize_pinecone():
-    global pc
-    if pc is None:
-        pc = Pinecone(api_key=PINECONE_API_KEY)
-        spec = ServerlessSpec(cloud="gcp", region="europe-west4")
-        existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+def init_pinecone(
+    api_key: str,
+    idx_name: str,
+    embd_dim: int,
+    sim_metric: str,
+) -> Index:
+    """Setup pinecone index"""
+    pc = Pinecone(api_key=api_key)
+    spec = ServerlessSpec(cloud="gcp", region="europe-west4")
+    existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
 
-        if PINECONE_INDEX_NAME not in existing_indexes:
-            pc.create_index(
-                PINECONE_INDEX_NAME,
-                dimension=EMBEDDING_MODEL_DIM,
-                metric=SIM_METRIC,
-                spec=spec,
-            )
-            while not pc.describe_index(PINECONE_INDEX_NAME).status["ready"]:
-                time.sleep(1)
+    if idx_name not in existing_indexes:
+        pc.create_index(
+            idx_name,
+            dimension=embd_dim,
+            metric=sim_metric,
+            spec=spec,
+        )
+        while not pc.describe_index(idx_name).status["ready"]:
+            time.sleep(1)
+
+    return pc.Index(idx_name)
 
 
 def get_vector_db():
-    initialize_pinecone()
-    return pc.Index(PINECONE_INDEX_NAME)
+    global idx
+    if idx is None:
+        # initialize pinecone
+        idx = init_pinecone(
+            PINECONE_API_KEY, PINECONE_INDEX_NAME, EMBEDDING_MODEL_DIM, SIM_METRIC
+        )
+    return idx
 
 
 VectorDbSession = Annotated[Index, Depends(get_vector_db)]
